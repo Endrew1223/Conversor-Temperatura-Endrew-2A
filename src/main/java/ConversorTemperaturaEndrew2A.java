c78bceba0319f1ef154046a34686ac4740507f2c
Endrew Henrique <endrewhenriquelimaoliveira@gmail.com>
2025-04-29 09:41:43 -0300

    Primeiro Commit

---
+++ src/main/java/ConversorTemperaturaEndrew2A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author 2024105070037
+ */
+public class ConversorTemperaturaEndrew2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
